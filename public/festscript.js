77ec433317d980b2d4b85f1d105fb15860275ea4
maruramulumba <marura.mulumba@asos.com>
2020-05-19 13:17:22 +0100

    render map but server issues

--- public/festscript.js
+++ public/festscript.js
@@ -6,34 +6,38 @@ console.log("hello world :o");
 // define variables that reference elements on our page
 
 const lineup = document.getElementById("fest");
+const mapdata = document.getElementById("map");
 
 function renderImage(fest) {
   const pageposter = document.createElement("img");
   pageposter.src = fest[2];
   pageposter.type = "image";
-//insert map data from json fest[3]??
-  lineup.appendChild(pageposter);
+
+lineup.appendChild(pageposter);
+mapdata.innerHTML= fest[3];
 }
 
+// function renderMap(fest) {
+// //var mapdata=document.createElement('map');
 
-//render map data for image
+// //lineup.appendChild(mapdata);
+// }
 
 
 fetch("/fests")
   .then(response => response.json()) // parse the JSON from the server
   .then(fests => {
-    
+    console.log(fests)
    var page=lineup.firstElementChild.innerHTML;
 
 fests.forEach(festival => {
   if ((festival.toString()).includes(page)) {
         lineup.firstElementChild.remove();
         renderImage(fests[2]);
-        
+        //renderMap(fests[1]);
+       
       };
+ 
 });
 
-//apply mapping to image
-
-
-  });
+});
--- server.js
+++ server.js
@@ -10,19 +10,37 @@ const app = express();
 
 const fests = [
   [
-    "parklife.html",
-    "https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2Fparklifelogo.jpg?v=1589240296908"
+    `"parklife.html"`,
+    `https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2Fparklifelogo.jpg?v=1589240296908`
   ],
   [
-    "citadel.html",
-    "https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2FCitadel-Festival-Logo.jpg?v=1589236183902"
+    `citadel.html`,
+    `https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2FCitadel-Festival-Logo.jpg?v=1589236183902`
   ],
   [
-    "lovebox.html",
-    "https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2FLOVEBOX2020.png?v=1589236146081",
-    "https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2Floveboxlineup.png?v=1589289393437"
+    `lovebox.html`,
+    `https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2FLOVEBOX2020.png?v=1589236146081`,
+    `https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2Floveboxlineup.png?v=1589289393437`,
+    
   ]
 ];
+
+
+    //`<map name="lovebox-map"> <area target="" alt="tyler the creator " title="tyler the creator " href="" coords="62,268,1020,387" shape="rect"> <area target="" alt="khalid " title="khalid " href="" coords="1019,510,598,388,337,480,206,446,314,508,468,596" shape="0"> <area target="" alt="Disclosure " title="Disclosure " href="" coords="597,511,60,387,360,658" shape="0"><area target="" alt="anderson paak " title="anderson paak " href="" coords="649,816,64,574,657,587,1009,687" shape="0"><area target="" alt="Charli xcx" title="Charli xcx" href="" coords="651,573,1019,694" shape="0"><area target="" alt="Fka twigs" title="Fka twigs" href="" coords="650,695,1018,815" shape="0"></map>`
+
+
+/* <img src="loveboxlineup.png" usemap="#image-map">
+
+<map name="image-map">
+    <area target="" alt="tyler the creator " title="tyler the creator " href="" coords="62,268,1020,387" shape="rect">
+    <area target="" alt="khalid " title="khalid " href="" coords="1019,510,598,388,337,480,206,446,314,508,468,596" shape="0">
+    <area target="" alt="Disclosure " title="Disclosure " href="" coords="597,511,60,387,360,658" shape="0">
+    <area target="" alt="anderson paak " title="anderson paak " href="" coords="649,816,64,574,657,587,1009,687" shape="0">
+    <area target="" alt="Charli xcx" title="Charli xcx" href="" coords="651,573,1019,694" shape="0">
+    <area target="" alt="Fka twigs" title="Fka twigs" href="" coords="650,695,1018,815" shape="0">
+</map> */
+
+
 
 // make all the files in 'public' available
 //https://expres sjs.com/en/starter/static-files.html
@@ -44,6 +62,5 @@ app.get("/fests", (request, response) => {
 
 });
 
-  
 
 app.listen(3000)
